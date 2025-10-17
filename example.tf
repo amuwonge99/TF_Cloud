a13226865d6b6269d7d331b8d53162e12c1efdc4
Augustine <105420895+amuwonge99@users.noreply.github.com>
2025-10-17 20:13:14 +0100

    Initialize Terraform with AWS provider and VPC

--- example.tf
+++ example.tf
@@ -1,6 +1,18 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
+
 resource "aws_vpc" "gus_test" {
   cidr_block = "10.0.0.0/24"
 }
